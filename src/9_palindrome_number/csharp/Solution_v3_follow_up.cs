750c9ae6884a243625a67fc070a2153cdfa7fd2d
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-09 21:14:23 +0600

    Solution - "9. Palindrome Number" v3 follow up

---
+++ src/9_palindrome_number/csharp/Solution_v3_follow_up.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0)
+            {
+                return false;
+            }
+
+            int initial = x;
+            int reverse = 0;
+
+            while (initial != 0)
+            {
+                reverse = reverse * 10 + initial % 10;
+                initial /= 10;
+            }
+
+            if (x == reverse)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
