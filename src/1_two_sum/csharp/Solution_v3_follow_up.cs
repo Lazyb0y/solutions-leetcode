3eb1f780745bf36893c5fff9bba4aba66ce1f548
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-09 20:43:48 +0600

    Solution - "1. Two Sum" v3 follow up

---
+++ src/1_two_sum/csharp/Solution_v3_follow_up.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (map.ContainsKey(target - nums[i]))
+                {
+                    return new int[] {i, map[target - nums[i]]};
+                }
+
+                map[nums[i]] = i;
+            }
+
+            return new int[] {0, 1};
+        }
+    }
+}
