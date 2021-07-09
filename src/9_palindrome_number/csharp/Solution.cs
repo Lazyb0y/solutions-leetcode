a77598d97d38ba08898e2e7515f506112aeb3581
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-09 17:09:00 +0600

    Solution - "9. Palindrome Number"

---
+++ src/9_palindrome_number/csharp/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            string inputString = x.ToString();
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (inputString[i] != inputString[inputString.Length - i - 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
