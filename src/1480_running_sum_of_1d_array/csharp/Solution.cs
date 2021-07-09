cddb8204c17b3f24033efd2bb802538b6a604a85
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-09 15:45:04 +0600

    Solution - "1480. Running Sum of 1d Array"

---
+++ src/1480_running_sum_of_1d_array/csharp/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            int[] runningSum = new int[nums.Length];
+
+            runningSum[0] = nums[0];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                runningSum[i] = runningSum[i - 1] + nums[i];
+            }
+
+            return runningSum;
+        }
+    }
+}
