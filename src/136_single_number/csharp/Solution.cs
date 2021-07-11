aa6fd81901700fbe55ad1091c6e3c030194c0a5f
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-11 20:47:51 +0600

    Solution - "136. Single Number"

---
+++ src/136_single_number/csharp/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            Hashtable hashtable = new Hashtable();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (hashtable.ContainsKey(nums[i]))
+                {
+                    hashtable.Remove(nums[i]);
+                }
+                else
+                {
+                    hashtable.Add(nums[i], null);
+                }
+            }
+
+            foreach (DictionaryEntry hashEntry in hashtable)
+            {
+                return (int) hashEntry.Key;
+            }
+
+            return nums[0];
+        }
+    }
+}
