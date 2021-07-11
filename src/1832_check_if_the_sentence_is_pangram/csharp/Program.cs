6ddb546704ad064d94bad0ba506ced6dc9d348f3
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-11 16:22:22 +0600

    Solution - "1832. Check if the Sentence Is Pangram"

---
+++ src/1832_check_if_the_sentence_is_pangram/csharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LeetCodeSolutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution().MaximumWealth(new int[][]
+            {
+                new[] {1, 2, 3},
+                new[] {4, 5, 6}
+            });
+        }
+    }
+}
