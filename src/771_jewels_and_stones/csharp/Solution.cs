cb5f822047f0d1d3532ebd73177c3eb4b5d026a7
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-11 14:35:22 +0600

    Solution - "771. Jewels and Stones"

---
+++ src/771_jewels_and_stones/csharp/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int NumJewelsInStones(string jewels, string stones)
+        {
+            int jewelCount = 0;
+
+            for (int i = 0; i < stones.Length; i++)
+            {
+                for (int j = 0; j < jewels.Length; j++)
+                {
+                    if (stones[i] == jewels[j])
+                    {
+                        jewelCount++;
+                        break;
+                    }
+                }
+            }
+
+            return jewelCount;
+        }
+    }
+}
