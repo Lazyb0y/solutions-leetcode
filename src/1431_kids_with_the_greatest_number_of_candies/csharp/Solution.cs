bbc5d902a3e51edcef65e98f6c1c3f3043239744
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-11 19:51:05 +0600

    Solution - "1431. Kids With the Greatest Number of Candies"

---
+++ src/1431_kids_with_the_greatest_number_of_candies/csharp/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            int max = 0;
+            for (int i = 0; i < candies.Length; i++)
+            {
+                if (candies[i] > max)
+                {
+                    max = candies[i];
+                }
+            }
+
+            IList<bool> result = new bool[candies.Length];
+            for (int i = 0; i < candies.Length; i++)
+            {
+                if (candies[i] + extraCandies >= max)
+                {
+                    result[i] = true;
+                }
+                else
+                {
+                    result[i] = false;
+                }
+            }
+
+            return result;
+        }
+    }
+}
