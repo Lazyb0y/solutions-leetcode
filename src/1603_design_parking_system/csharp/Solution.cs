1997129293266a49617116836cc030f2656f73a6
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-11 20:13:32 +0600

    Solution - "1603. Design Parking System"

---
+++ src/1603_design_parking_system/csharp/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeSolutions
+{
+    public class ParkingSystem
+    {
+        private int[] _parkingAvailable = new int[4];
+
+        public ParkingSystem(int big, int medium, int small)
+        {
+            _parkingAvailable[1] = big;
+            _parkingAvailable[2] = medium;
+            _parkingAvailable[3] = small;
+        }
+
+        public bool AddCar(int carType)
+        {
+            if (_parkingAvailable[carType] > 0)
+            {
+                _parkingAvailable[carType]--;
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    /**
+     * Your ParkingSystem object will be instantiated and called as such:
+     * ParkingSystem obj = new ParkingSystem(big, medium, small);
+     * bool param_1 = obj.AddCar(carType);
+     */
+}
