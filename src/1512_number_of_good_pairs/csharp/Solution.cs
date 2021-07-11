88a13b76890ea8dd624dd4ebc57d3f1ea4589f84
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-11 16:47:48 +0600

    Solution - "1512. Number of Good Pairs"

---
+++ src/1512_number_of_good_pairs/csharp/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int NumIdenticalPairs(int[] nums)
+        {
+            int count = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j] && i < j)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
