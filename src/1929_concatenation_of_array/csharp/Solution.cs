dca52b86bd6035c0b517dfb4035673d3c60e02ef
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-11 13:00:26 +0600

    Solution - "1929. Concatenation of Array"

---
+++ src/1929_concatenation_of_array/csharp/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int[] GetConcatenation(int[] nums)
+        {
+            int[] output = new int[nums.Length + nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                output[i] = output[nums.Length + i] = nums[i];
+            }
+
+            return output;
+        }
+    }
+}
