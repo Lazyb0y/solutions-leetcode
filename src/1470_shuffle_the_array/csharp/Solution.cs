9bdd1aefc9da5ea5db91186547ed47d2b81b3deb
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-11 17:34:57 +0600

    Solution - "1470. Shuffle the Array"

---
+++ src/1470_shuffle_the_array/csharp/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int[] Shuffle(int[] nums, int n)
+        {
+            int[] xElements = new int[n];
+            int[] yElements = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                xElements[i] = nums[i];
+                yElements[i] = nums[i + n];
+            }
+
+            int[] output = new int[nums.Length];
+            int index = 0;
+            for (int i = 0; i < n; i++)
+            {
+                output[index] = xElements[i];
+                index++;
+                output[index] = yElements[i];
+                index++;
+            }
+
+            return output;
+        }
+    }
+}
