01f11ec6302b5a47c85444ecc7c532ba54aa02f4
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-11 14:22:41 +0600

    Solution - "1672. Richest Customer Wealth"

---
+++ src/1672_richest_customer_wealth/csharp/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int MaximumWealth(int[][] accounts)
+        {
+            int maximumWealth = 0;
+
+            for (int i = 0; i < accounts.GetLength(0); i++)
+            {
+                int wealth = 0;
+                for (int j = 0; j < accounts[i].Length; j++)
+                {
+                    wealth += accounts[i][j];
+                }
+
+                if (maximumWealth < wealth)
+                {
+                    maximumWealth = wealth;
+                }
+            }
+
+            return maximumWealth;
+        }
+    }
+}
