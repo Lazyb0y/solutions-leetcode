3cbfa878efb7994f7f85a633c6f09fd31f756eab
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-12 19:02:57 +0600

    Solution - "1281. Subtract the Product and Sum of Digits of an Integer"

---
+++ src/1281_subtract_the_product_and_sum_of_digits_of_an_integer/csharp/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int SubtractProductAndSum(int n)
+        {
+            int sum = 0;
+            int product = 1;
+
+            while (n > 0)
+            {
+                var remainder = n % 10;
+                n = n / 10;
+
+                sum += remainder;
+                product *= remainder;
+            }
+
+            return product - sum;
+        }
+    }
+}
