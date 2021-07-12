4f85968266710c8627823d9d5472b7e205fc887a
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-12 13:43:49 +0600

    Solution - "905. Sort Array By Parity"

---
+++ src/905_sort_array_by_parity/csharp/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int[] SortArrayByParity(int[] nums)
+        {
+            List<int> oddNumbers = new List<int>();
+            List<int> evenNumbers = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if ((nums[i] & 1) == 0)
+                {
+                    evenNumbers.Add(nums[i]);
+                }
+                else
+                {
+                    oddNumbers.Add(nums[i]);
+                }
+            }
+
+            int[] output = new int[nums.Length];
+
+            int iterator = 0;
+
+            foreach (var evenNumber in evenNumbers)
+            {
+                output[iterator] = evenNumber;
+                iterator++;
+            }
+
+            foreach (var oddNumber in oddNumbers)
+            {
+                output[iterator] = oddNumber;
+                iterator++;
+            }
+
+            return output;
+        }
+    }
+}
