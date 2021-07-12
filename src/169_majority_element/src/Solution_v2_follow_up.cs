00c52001f61aa9479d898062498cbd342571853d
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-12 11:59:23 +0600

    Solution - "169. Majority Element" v2 follow up

---
+++ src/169_majority_element/src/Solution_v2_follow_up.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int MajorityElement(int[] nums)
+        {
+            int candidate = nums[0];
+            int counter = 1;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (counter == 0)
+                {
+                    candidate = nums[i];
+                }
+
+                if (nums[i] == candidate)
+                {
+                    counter++;
+                }
+                else
+                {
+                    counter--;
+                }
+            }
+
+            return candidate;
+        }
+    }
+}
