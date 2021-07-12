addbcf0396f576fba2379995d2267bf242e5cd6c
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-12 12:28:24 +0600

    Solution - "961. N-Repeated Element in Size 2N Array" v2

---
+++ src/961_ n-repeated_element_in_size_2n_array/csharp/Solution_v2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int RepeatedNTimes(int[] nums)
+        {
+            HashSet<int> hashSet = new HashSet<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (hashSet.Contains(nums[i]))
+                {
+                    return nums[i];
+                }
+
+                hashSet.Add(nums[i]);
+            }
+
+            return nums[1];
+        }
+    }
+}
