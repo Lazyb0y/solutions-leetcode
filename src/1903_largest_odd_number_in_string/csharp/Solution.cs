f23a3991f420e64a915334974619cc582faebc52
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-12 20:40:30 +0600

    Solution - "1903. Largest Odd Number in String"

---
+++ src/1903_largest_odd_number_in_string/csharp/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public string LargestOddNumber(string num)
+        {
+            for (int i = num.Length - 1; i >= 0; i--)
+            {
+                switch (num[i])
+                {
+                    case '1':
+                    case '3':
+                    case '5':
+                    case '7':
+                    case '9':
+                        return num.Substring(0, i + 1);
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
