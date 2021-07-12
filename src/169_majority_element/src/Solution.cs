b91f543bd14e4ce8d2ce785991ed6ce0e00394fe
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-12 11:27:29 +0600

    Solution - "169. Majority Element"

---
+++ src/169_majority_element/src/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int MajorityElement(int[] nums)
+        {
+            Dictionary<int, int> counts = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (counts.ContainsKey(nums[i]))
+                {
+                    counts[nums[i]]++;
+                }
+                else
+                {
+                    counts.Add(nums[i], 1);
+                }
+            }
+
+            int maxCount = 0;
+            int maxNumber = 0;
+            foreach (var count in counts)
+            {
+                if (count.Value > maxCount)
+                {
+                    maxNumber = count.Key;
+                    maxCount = count.Value;
+                }
+            }
+
+            return maxNumber;
+        }
+    }
+}
