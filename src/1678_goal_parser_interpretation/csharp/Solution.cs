992198f5cc4f0bde79653fddef247bd0a6a16f67
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-12 17:14:02 +0600

    Solution - "1678. Goal Parser Interpretation"

---
+++ src/1678_goal_parser_interpretation/csharp/Solution.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public string Interpret(string command)
+        {
+            string result = string.Empty;
+
+            string commandPart = string.Empty;
+            for (int i = 0; i < command.Length;)
+            {
+                while (true)
+                {
+                    if (commandPart == "G")
+                    {
+                        result += "G";
+                        commandPart = string.Empty;
+                        break;
+                    }
+
+                    if (commandPart == "()")
+                    {
+                        result += "o";
+                        commandPart = string.Empty;
+                        break;
+                    }
+
+                    if (commandPart == "(al)")
+                    {
+                        result += "al";
+                        commandPart = string.Empty;
+                        break;
+                    }
+
+                    commandPart = commandPart + command[i];
+                    i++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
