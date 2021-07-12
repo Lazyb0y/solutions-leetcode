d2af8a0975d2f05b43e825c8ddba3012fdeb9349
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-12 21:15:26 +0600

    Solution - "1389. Create Target Array in the Given Order"

---
+++ src/1389_create_target_array_in_the_given_order/csharp/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int[] CreateTargetArray(int[] nums, int[] index)
+        {
+            int[] result = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i > index[i])
+                {
+                    for (int j = nums.Length - 1; j > index[i]; j--)
+                    {
+                        result[j] = result[j - 1];
+                    }
+                }
+
+                result[index[i]] = nums[i];
+            }
+
+            return result;
+        }
+    }
+}
