2bbe3dcc73efcc3b2577f1ea04d60bced4c81e2c
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-12 20:53:32 +0600

    Solution - "1720. Decode XORed Array"

---
+++ src/1720_decode_xored_array/csharp/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int[] Decode(int[] encoded, int first)
+        {
+            int[] decode = new int[encoded.Length + 1];
+            decode[0] = first;
+
+            for (int i = 0; i < encoded.Length; i++)
+            {
+                decode[i + 1] = decode[i] ^ encoded[i];
+            }
+
+            return decode;
+        }
+    }
+}
