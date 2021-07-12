6428a0ddf900fb14d8724714544aff11c88a5a46
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-12 12:22:00 +0600

    Solution - "961. N-Repeated Element in Size 2N Array"

---
+++ src/961_ n-repeated_element_in_size_2n_array/csharp/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int RepeatedNTimes(int[] nums)
+        {
+            Dictionary<int, int> count = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (count.ContainsKey(nums[i]))
+                {
+                    count[nums[i]]++;
+                }
+                else
+                {
+                    count.Add(nums[i], 1);
+                }
+            }
+
+            int n = nums.Length / 2;
+
+            foreach (var item in count)
+            {
+                if (item.Value == n)
+                {
+                    return item.Key;
+                }
+            }
+
+            return nums[0];
+        }
+    }
+}
