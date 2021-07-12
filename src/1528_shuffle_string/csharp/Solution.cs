80389a4a78be6e87ca27bd4f075a69a4a33f3379
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-12 18:54:25 +0600

    Solution - "1528. Shuffle String"

---
+++ src/1528_shuffle_string/csharp/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public string RestoreString(string s, int[] indices)
+        {
+            char[] sorted = new char[s.Length];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                sorted[indices[i]] = s[i];
+            }
+
+            return new string(sorted);
+        }
+    }
+}
