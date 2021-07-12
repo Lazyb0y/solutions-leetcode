afbe42ee4b3309dc914b575155dda0ff99f7898b
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-12 15:31:22 +0600

    Solution - "728. Self Dividing Numbers"

---
+++ src/728_self_dividing_numbers/csharp/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public IList<int> SelfDividingNumbers(int left, int right)
+        {
+            IList<int> self = new List<int>();
+
+            for (int i = left; i <= right; i++)
+            {
+                int digit = i;
+
+                bool selfDividing = true;
+                while (digit != 0)
+                {
+                    int remainder = digit % 10;
+
+                    if (remainder == 0)
+                    {
+                        selfDividing = false;
+                        break;
+                    }
+
+                    if (i % remainder != 0)
+                    {
+                        selfDividing = false;
+                        break;
+                    }
+
+                    digit = digit / 10;
+                }
+
+                if (selfDividing)
+                {
+                    self.Add(i);
+                }
+            }
+
+            return self;
+        }
+    }
+}
