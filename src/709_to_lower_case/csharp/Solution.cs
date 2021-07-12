7a9061019b9e300cad8f70786ee54ed2dc4026d5
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-12 12:54:29 +0600

    Solution - "709. To Lower Case"

---
+++ src/709_to_lower_case/csharp/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public string ToLowerCase(string s)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if ('A' <= s[i] && s[i] <= 'Z')
+                {
+                    builder.Append(Convert.ToChar(s[i] + 32));
+                }
+                else
+                {
+                    builder.Append(s[i]);
+                }
+            }
+
+            return builder.ToString();
+        }
+    }
+}
