aecbb98286a706dedf9c6b00a63ae9243de9ce08
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-12 14:17:54 +0600

    Solution - "905. Sort Array By Parity" v2

---
+++ src/905_sort_array_by_parity/csharp/Solution_v2.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int[] SortArrayByParity(int[] nums)
+        {
+            int evenPointer = 0;
+            int oddPointer = nums.Length - 1;
+
+            int currentIndex = 0;
+            while (evenPointer < oddPointer)
+            {
+                if ((nums[currentIndex] & 1) == 0)
+                {
+                    evenPointer++;
+                    currentIndex++;
+                }
+                else
+                {
+                    int temp = nums[currentIndex];
+                    nums[currentIndex] = nums[oddPointer];
+                    nums[oddPointer] = temp;
+
+                    oddPointer--;
+                }
+            }
+
+            return nums;
+        }
+    }
+}
