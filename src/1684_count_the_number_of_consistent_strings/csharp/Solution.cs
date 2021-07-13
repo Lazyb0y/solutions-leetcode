c6fbeaf819b372b3450369c1ad3163b2fd5ceba8
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-13 18:40:50 +0600

    Solution - "1684. Count the Number of Consistent Strings"

---
+++ src/1684_count_the_number_of_consistent_strings/csharp/Solution.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int CountConsistentStrings(string allowed, string[] words)
+        {
+            int consistentCount = 0;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                bool isConsistent = true;
+
+                for (int j = 0; j < words[i].Length; j++)
+                {
+                    bool found = false;
+                    for (int k = 0; k < allowed.Length; k++)
+                    {
+                        if (words[i][j] == allowed[k])
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found)
+                    {
+                        isConsistent = false;
+                        break;
+                    }
+                }
+
+                if (isConsistent)
+                {
+                    consistentCount++;
+                }
+            }
+
+            return consistentCount;
+        }
+    }
+}
