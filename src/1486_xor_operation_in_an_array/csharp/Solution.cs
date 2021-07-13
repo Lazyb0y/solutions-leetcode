d32f5b63a94246f3a7fce7b2e67b01367b691dec
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-13 20:07:40 +0600

    Solution - "1486. XOR Operation in an Array"

---
+++ src/1486_xor_operation_in_an_array/csharp/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int XorOperation(int n, int start)
+        {
+            int result = start;
+
+            for (int i = 1; i < n; i++)
+            {
+                result ^= start + 2 * i;
+            }
+
+            return result;
+        }
+    }
+}
