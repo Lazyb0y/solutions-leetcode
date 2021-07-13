6620b47b9f83860e361ee4c094f8d36fa5fd50e9
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-13 16:05:28 +0600

    Solution - "1290. Convert Binary Number in a Linked List to Integer"

---
+++ src/1290_convert_binary_number_in_a_linked_list_to-integer/csharp/Solution.cs
@@ -0,0 +1,45 @@
+﻿namespace LeetCodeSolutions
+{
+    //public class ListNode
+    //{
+    //    public int val;
+    //    public ListNode next;
+
+    //    public ListNode(int val = 0, ListNode next = null)
+    //    {
+    //        this.val = val;
+    //        this.next = next;
+    //    }
+    //}
+
+    public class Solution
+    {
+        public int GetDecimalValue(ListNode head)
+        {
+            int[] binary = new int[30];
+
+            int length = 0;
+            for (int i = 0; i <= 30; i++)
+            {
+                if (head == null)
+                {
+                    length = i;
+                    break;
+                }
+
+                binary[i] = head.val;
+                head = head.next;
+            }
+
+            int decimalValue = 0;
+            int factor = 1;
+            for (int i = length - 1; i >= 0; i--)
+            {
+                decimalValue += binary[i] * factor;
+                factor *= 2;
+            }
+
+            return decimalValue;
+        }
+    }
+}
