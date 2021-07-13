2e780b5bdb9eb91e4badc25bb66ad99383eb1266
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-13 15:00:17 +0600

    Solution - "1816. Truncate Sentence"

---
+++ src/1816_truncate_sentence/csharp/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public string TruncateSentence(string s, int k)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (k == 0)
+                {
+                    return s.Substring(0, i - 1);
+                }
+
+                if (s[i] == ' ')
+                {
+                    k--;
+                }
+            }
+
+            return s;
+        }
+    }
+}
