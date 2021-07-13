9e6a66ba00d5826fbcf3c94fb268053d7e56e7c1
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-13 17:01:40 +0600

    Solution - "1859. Sorting the Sentence"

---
+++ src/1859_sorting_the_sentence/csharp/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public string SortSentence(string s)
+        {
+            string[] parts = s.Split(' ');
+
+            string[] resultParts = new string[parts.Length];
+
+            for (int i = 0; i < parts.Length; i++)
+            {
+                int index = Int32.Parse(parts[i][parts[i].Length - 1] + "") - 1;
+                resultParts[index] = parts[i].Substring(0, parts[i].Length - 1);
+            }
+
+            return string.Join(" ", resultParts);
+        }
+    }
+}
