62d96af98f46cc5461a874137b581f797ed4792e
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-13 15:14:36 +0600

    Solution - "1365. How Many Numbers Are Smaller Than the Current Number"

---
+++ src/1365_how_many_numbers_are_smaller_than_the_current_number/csharp/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int[] SmallerNumbersThanCurrent(int[] nums)
+        {
+            Dictionary<int, int> uniqueCount = new Dictionary<int, int>();
+
+            int[] output = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (uniqueCount.ContainsKey(nums[i]))
+                {
+                    output[i] = uniqueCount[nums[i]];
+                }
+                else
+                {
+                    int count = 0;
+                    for (int j = 0; j < nums.Length; j++)
+                    {
+                        if (i != j && nums[j] < nums[i])
+                        {
+                            count++;
+                        }
+                    }
+
+                    output[i] = count;
+                    uniqueCount.Add(nums[i], count);
+                }
+            }
+
+            return output;
+        }
+    }
+}
