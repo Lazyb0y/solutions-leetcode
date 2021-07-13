ce1bb03b9bcbfebffdf44aa355dd4b64d96f2611
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-13 20:22:10 +0600

    Solution - "557. Reverse Words in a String III"

---
+++ src/557_reverse_words_in_a_string_iii/csharp/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public string ReverseWords(string s)
+        {
+            string[] words = s.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                int head = 0;
+                int tail = words[i].Length - 1;
+
+                char[] reversedWord = new char[words[i].Length];
+                while (head <= tail)
+                {
+                    reversedWord[head] = words[i][tail];
+                    reversedWord[tail] = words[i][head];
+                    head++;
+                    tail--;
+                }
+
+                words[i] = new string(reversedWord);
+            }
+
+            return string.Join(" ", words);
+        }
+    }
+}
