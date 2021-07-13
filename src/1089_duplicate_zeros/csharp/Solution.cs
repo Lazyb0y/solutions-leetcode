e43c0f8bbaa5d4883b346249b5aa41ebea167200
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-13 19:27:01 +0600

    Solution - "1089. Duplicate Zeros"

---
+++ src/1089_duplicate_zeros/csharp/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public void DuplicateZeros(int[] arr)
+        {
+            int currentIndex = 0;
+
+            while (currentIndex < arr.Length - 1)
+            {
+                if (arr[currentIndex] == 0)
+                {
+                    for (int i = arr.Length - 1; i > currentIndex; i--)
+                    {
+                        arr[i] = arr[i - 1];
+                    }
+
+                    currentIndex++;
+                }
+
+                currentIndex++;
+            }
+        }
+    }
+}
