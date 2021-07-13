48a19a407285971ce62ff79fce9469821bcf9bcc
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-13 16:37:55 +0600

    Solution - "1588. Sum of All Odd Length Subarrays"

---
+++ src/1588_sum_of_all_odd_length_subarrays/csharp/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int SumOddLengthSubarrays(int[] arr)
+        {
+            int sum = 0;
+            int length = 1;
+
+            while (length <= arr.Length)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (length + i > arr.Length)
+                    {
+                        break;
+                    }
+
+                    for (int j = i; j < length + i; j++)
+                    {
+                        sum += arr[j];
+                    }
+                }
+
+                length = length + 2;
+            }
+
+            return sum;
+        }
+    }
+}
