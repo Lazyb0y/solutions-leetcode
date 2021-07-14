417b9e16b743d02811eacc53e89f0fee76cffd98
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-14 11:15:21 +0600

    Solution - "1844. Replace All Digits with Characters"

---
+++ src/1844_replace_all_digits_with_characters/csharp/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public string ReplaceDigits(string s)
+        {
+            char[] resultChars = new char[s.Length];
+
+            for (int i = 0, j = 1; i < s.Length - 1; i += 2, j += 2)
+            {
+                resultChars[i] = s[i];
+                resultChars[j] = (char) (s[i] + int.Parse(s[j] + ""));
+            }
+
+            if (s.Length % 2 != 0)
+            {
+                resultChars[s.Length - 1] = s[s.Length - 1];
+            }
+
+            return new string(resultChars);
+        }
+    }
+}
