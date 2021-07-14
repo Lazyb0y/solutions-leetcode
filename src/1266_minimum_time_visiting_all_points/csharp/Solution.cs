c459955d84199c5699bc2715598f179a35013bcc
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-14 16:38:32 +0600

    Solution - "1266. Minimum Time Visiting All Points"

---
+++ src/1266_minimum_time_visiting_all_points/csharp/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int MinTimeToVisitAllPoints(int[][] points)
+        {
+            int minimumTime = 0;
+
+            for (int i = 0; i < points.Length - 1; i++)
+            {
+                int xDistance = Math.Abs(points[i + 1][0] - points[i][0]);
+                int yDistance = Math.Abs(points[i + 1][1] - points[i][1]);
+
+                minimumTime += Math.Max(xDistance, yDistance);
+            }
+
+            return minimumTime;
+        }
+    }
+}
