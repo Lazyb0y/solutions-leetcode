0c1b216add1d24092b18bf9076adce867e8d1fb9
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-14 10:51:52 +0600

    Solution - "1732. Find the Highest Altitude"

---
+++ src/1732_find_the_highest_altitude/csharp/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int LargestAltitude(int[] gain)
+        {
+            int largestAltitude = 0;
+            int currentAltitude = 0;
+
+            for (int i = 0; i < gain.Length; i++)
+            {
+                currentAltitude += gain[i];
+
+                if (currentAltitude > largestAltitude)
+                {
+                    largestAltitude = currentAltitude;
+                }
+            }
+
+            return largestAltitude;
+        }
+    }
+}
