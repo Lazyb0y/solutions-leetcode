fc03eb8be11d21924bfcd86f441d926a727040fe
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-15 15:21:46 +0600

    Solution - "1791. Find Center of Star Graph"

---
+++ src/1791_find_center_of_star_graph/csharp/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int FindCenter(int[][] edges)
+        {
+            HashSet<int> edgePoint = new HashSet<int> {edges[0][0], edges[0][1]};
+
+            if (edgePoint.Contains(edges[1][0]))
+            {
+                return edges[1][0];
+            }
+            else
+            {
+                return edges[1][1];
+            }
+        }
+    }
+}
