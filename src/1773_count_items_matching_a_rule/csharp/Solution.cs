23f26afd8ae9a5715976fef323c1430dce7a3258
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-15 15:49:38 +0600

    Solution - "1773. Count Items Matching a Rule"

---
+++ src/1773_count_items_matching_a_rule/csharp/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int CountMatches(IList<IList<string>> items, string ruleKey, string ruleValue)
+        {
+            int martchCount = 0;
+
+            foreach (var item in items)
+            {
+                switch (ruleKey)
+                {
+                    case "type":
+                        if (item[0] == ruleValue)
+                        {
+                            martchCount++;
+                        }
+
+                        break;
+                    case "color":
+                        if (item[1] == ruleValue)
+                        {
+                            martchCount++;
+                        }
+
+                        break;
+                    case "name":
+                        if (item[2] == ruleValue)
+                        {
+                            martchCount++;
+                        }
+
+                        break;
+                }
+            }
+
+            return martchCount;
+        }
+    }
+}
