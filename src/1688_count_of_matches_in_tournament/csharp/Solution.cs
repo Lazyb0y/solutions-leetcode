51ccce2e6fa22e14b03a0923ec0b1fc148bebde7
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-19 19:34:14 +0600

    Solution - "1688. Count of Matches in Tournament"

---
+++ src/1688_count_of_matches_in_tournament/csharp/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int NumberOfMatches(int n)
+        {
+            int numberOfMatches = 0;
+
+            while (n > 1)
+            {
+                if (n % 2 == 0)
+                {
+                    n = n / 2;
+                    numberOfMatches += n;
+                }
+                else
+                {
+                    n = (n - 1) / 2 + 1;
+                    numberOfMatches += n - 1;
+                }
+            }
+
+            return numberOfMatches;
+        }
+    }
+}
