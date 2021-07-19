ea3429394672a5c748b4ba6a4b0bb2fc37936356
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-19 19:08:00 +0600

    Solution - "1313. Decompress Run-Length Encoded List"

---
+++ src/1313_decompress_run_length_encoded_list/csharp/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int[] DecompressRLElist(int[] nums)
+        {
+            List<int> resultList = new List<int>();
+
+            for (int i = 0; i < nums.Length; i = i + 2)
+            {
+                for (int j = 0; j < nums[i]; j++)
+                {
+                    resultList.Add(nums[i + 1]);
+                }
+            }
+
+            return resultList.ToArray();
+        }
+    }
+}
