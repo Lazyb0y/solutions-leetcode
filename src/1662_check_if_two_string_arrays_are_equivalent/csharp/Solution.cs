ff7c5a82fa96160fc523d32d6c7c104ae8d61399
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-19 19:41:01 +0600

    Solution - "1662. Check If Two String Arrays are Equivalent"

---
+++ src/1662_check_if_two_string_arrays_are_equivalent/csharp/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public bool ArrayStringsAreEqual(string[] word1, string[] word2)
+        {
+            StringBuilder string1 = new StringBuilder();
+            StringBuilder string2 = new StringBuilder();
+
+            foreach (var word in word1)
+            {
+                string1.Append(word);
+            }
+
+            foreach (var word in word2)
+            {
+                string2.Append(word);
+            }
+
+            return string1.Length == string2.Length && string1.ToString() == string2.ToString();
+        }
+    }
+}
