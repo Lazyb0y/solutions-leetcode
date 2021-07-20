3c36af5119916a058b3d8469eeaa612655348d27
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-20 14:59:31 +0600

    Solution - "448. Find All Numbers Disappeared in an Array"

---
+++ src/448_find_all_numbers_disappeared_in_an_array/csharp/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public IList<int> FindDisappearedNumbers(int[] nums)
+        {
+            HashSet<int> numSet = new HashSet<int>();
+
+            for (int i = 1; i <= nums.Length; i++)
+            {
+                numSet.Add(i);
+            }
+
+            foreach (var num in nums)
+            {
+                if (numSet.Contains(num))
+                {
+                    numSet.Remove(num);
+                }
+            }
+
+            return numSet.ToArray();
+        }
+    }
+}
