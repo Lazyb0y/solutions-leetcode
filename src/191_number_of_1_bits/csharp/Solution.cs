7f378c6a094205111ba8ed2ccc40d9f85a085040
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-20 15:59:29 +0600

    Solution - "191. Number of 1 Bits"

---
+++ src/191_number_of_1_bits/csharp/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int HammingWeight(uint n)
+        {
+            string nBinary = Convert.ToString(n, 2);
+
+            int count = 0;
+            for (int i = 0; i < nBinary.Length; i++)
+            {
+                if (nBinary[i] == '1')
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
