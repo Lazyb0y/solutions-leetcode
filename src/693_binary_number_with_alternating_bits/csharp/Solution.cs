f209a48fe970fc666c6f36b5336a6202793f8e4a
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-20 16:25:26 +0600

    Solution - "693. Binary Number with Alternating Bits"

---
+++ src/693_binary_number_with_alternating_bits/csharp/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public bool HasAlternatingBits(int n)
+        {
+            string nBitString = Convert.ToString(n, 2);
+
+            char last = nBitString[0];
+            for (int i = 1; i < nBitString.Length; i++)
+            {
+                if (last == nBitString[i])
+                {
+                    return false;
+                }
+
+                last = nBitString[i];
+            }
+
+            return true;
+        }
+    }
+}
