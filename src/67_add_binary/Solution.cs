ae278aa6127d529c3663da2731be7312c57d6d0a
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-24 00:40:56 +0600

    Solution - "67. Add Binary"

---
+++ src/67_add_binary/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public string AddBinary(string a, string b)
+        {
+            List<char> result = new List<char>();
+
+            int aIndex = a.Length - 1;
+            int bIndex = b.Length - 1;
+            int carry = 0;
+
+            while (aIndex >= 0 || bIndex >= 0 || carry > 0)
+            {
+                int aVal = aIndex >= 0 ? a[aIndex] - '0' : 0;
+                int bVal = bIndex >= 0 ? b[bIndex] - '0' : 0;
+
+                int sum = aVal + bVal + carry;
+
+                result.Add((char) (sum % 2 + '0'));
+                carry = sum / 2;
+
+                aIndex--;
+                bIndex--;
+            }
+
+            result.Reverse();
+            return new string(result.ToArray());
+        }
+    }
+}
