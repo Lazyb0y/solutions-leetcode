3f5b36a27afa85ddf56a5537537114f42fc46a84
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-23 20:15:13 +0600

    Solution - "1598. Crawler Log Folder"

---
+++ src/1598_crawler_log_folder/csharp/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int MinOperations(string[] logs)
+        {
+            int currentIndx = 0;
+
+            for (int i = 0; i < logs.Length; i++)
+            {
+                if (logs[i] == "../")
+                {
+                    if (currentIndx > 0)
+                    {
+                        currentIndx--;
+                    }
+                }
+                else if (logs[i] == "./")
+                {
+                    // Do nothing
+                }
+                else
+                {
+                    currentIndx++;
+                }
+            }
+
+            return currentIndx;
+        }
+    }
+}
