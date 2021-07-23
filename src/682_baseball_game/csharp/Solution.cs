9dce2d47acc3aab7309104fe70844e8750287ab4
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-23 19:51:04 +0600

    Solution - "682. Baseball Game"

---
+++ src/682_baseball_game/csharp/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int CalPoints(string[] ops)
+        {
+            List<int> scores = new List<int>();
+
+            for (int i = 0; i < ops.Length; i++)
+            {
+                switch (ops[i])
+                {
+                    case "+":
+                        scores.Add(scores[scores.Count - 1] + scores[scores.Count - 2]);
+                        break;
+
+                    case "D":
+                        scores.Add(2 * scores[scores.Count - 1]);
+                        break;
+
+                    case "C":
+                        scores.RemoveAt(scores.Count - 1);
+                        break;
+
+                    default:
+                        scores.Add(int.Parse(ops[i]));
+                        break;
+                }
+            }
+
+            return scores.Sum();
+        }
+    }
+}
