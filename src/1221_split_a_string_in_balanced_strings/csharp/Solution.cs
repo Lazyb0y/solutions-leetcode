b3e2bdd286040bb0bdcefc05a50f346591f6f111
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-23 23:50:53 +0600

    Solution - "1221. Split a String in Balanced Strings"

---
+++ src/1221_split_a_string_in_balanced_strings/csharp/Solution.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int BalancedStringSplit(string s)
+        {
+            int count = 0;
+
+            int lCount = 0;
+            int rCount = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'L')
+                {
+                    lCount++;
+                }
+                else
+                {
+                    rCount++;
+                }
+
+                if (lCount == 0 && rCount == 0)
+                {
+                    continue;
+                }
+
+                if (lCount == rCount)
+                {
+                    count++;
+                    lCount = 0;
+                    rCount = 0;
+                }
+            }
+
+            return count;
+        }
+    }
+}
