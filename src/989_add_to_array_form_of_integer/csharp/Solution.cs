ee551f373092ff4654821dbeaca28dd78f529235
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-24 00:18:15 +0600

    Solution - "989. Add to Array-Form of Integer"

---
+++ src/989_add_to_array_form_of_integer/csharp/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public IList<int> AddToArrayForm(int[] num, int k)
+        {
+            List<int> result = new List<int>();
+
+            int i = num.Length - 1;
+            int carry = 0;
+            while (i >= 0 || k > 0 || carry > 0)
+            {
+                int n = i >= 0 ? num[i] : 0;
+                int remainder = k % 10;
+                int sum = n + remainder + carry;
+                result.Add(sum % 10);
+
+                carry = sum / 10;
+                k /= 10;
+                i--;
+            }
+
+            result.Reverse();
+            return result;
+        }
+    }
+}
