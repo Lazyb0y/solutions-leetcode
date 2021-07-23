f6e85bfc58b68dafd9619c71d32207cd718d20fa
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-24 00:27:38 +0600

    Solution - "66. Plus One"

---
+++ src/66_plus_one/csharp/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            List<int> resultList = new List<int>();
+
+            int carry = 0;
+            int i = digits.Length - 1;
+            int one = 1;
+
+            while (i >= 0 || one > 0 || carry > 0)
+            {
+                int digit = i >= 0 ? digits[i] : 0;
+                int oneDigit = one % 10;
+                int sum = digit + oneDigit + carry;
+
+                resultList.Add(sum % 10);
+
+                one = one / 10;
+                carry = sum / 10;
+                i--;
+            }
+
+            resultList.Reverse();
+            return resultList.ToArray();
+        }
+    }
+}
