7cec6a3bef63f3ed49642b39db373443c6e10d51
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-23 19:33:04 +0600

    Solution - "1920. Build Array from Permutation"

---
+++ src/1920_build_array_from_permutation/csharp/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int[] BuildArray(int[] nums)
+        {
+            int[] ans = new int[nums.Length];
+
+            for (int i = 0; i < ans.Length; i++)
+            {
+                ans[i] = nums[nums[i]];
+            }
+
+            return ans;
+        }
+    }
+}
