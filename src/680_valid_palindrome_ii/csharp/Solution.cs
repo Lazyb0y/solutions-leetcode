18b628f54a4c43dc3f568a0b7075a773462e3f19
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-23 21:52:00 +0600

    Solution - "680. Valid Palindrome II"

---
+++ src/680_valid_palindrome_ii/csharp/Solution.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public bool ValidPalindrome(string s)
+        {
+            int head = 0;
+            int tail = s.Length - 1;
+
+            while (head < tail)
+            {
+                if (s[head] != s[tail])
+                {
+                    return IsPalindrome(s, head + 1, tail) || IsPalindrome(s, head, tail - 1);
+                }
+
+                head++;
+                tail--;
+            }
+
+            return true;
+        }
+
+        public bool IsPalindrome(string s, int head, int tail)
+        {
+            while (head < tail)
+            {
+                if (s[head] != s[tail])
+                {
+                    return false;
+                }
+
+                head++;
+                tail--;
+            }
+
+            return true;
+        }
+    }
+}
