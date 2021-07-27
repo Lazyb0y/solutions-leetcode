fa7665a901e07e3371368f63b46ef7771bf1bc1a
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-27 16:50:17 +0600

    Solution - "1827. Minimum Operations to Make the Array Increasing"

---
+++ src/1827_minimum_operations_to_make_the_array_increasing/csharp/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int MinOperations(int[] nums)
+        {
+            int minOperation = 0;
+
+            int index = 0;
+
+            while (index < nums.Length - 1)
+            {
+                if (nums[index] >= nums[index + 1])
+                {
+                    int toIncrement = nums[index] - nums[index + 1] + 1;
+                    nums[index + 1] += toIncrement;
+                    minOperation += toIncrement;
+                }
+                else
+                {
+                    index++;
+                }
+            }
+
+            return minOperation;
+        }
+    }
+}
