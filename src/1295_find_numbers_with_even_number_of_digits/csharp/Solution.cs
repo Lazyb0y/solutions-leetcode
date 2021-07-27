8811b05adedc4af1d36a539d0d920146212e5d00
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-27 14:15:18 +0600

    Solution - "1295. Find Numbers with Even Number of Digits"

---
+++ src/1295_find_numbers_with_even_number_of_digits/csharp/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int FindNumbers(int[] nums)
+        {
+            int evenCount = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int num = nums[i];
+
+                int length = 0;
+                while (num != 0)
+                {
+                    num = num / 10;
+                    length++;
+                }
+
+                if (length % 2 == 0)
+                {
+                    evenCount++;
+                }
+            }
+
+            return evenCount;
+        }
+    }
+}
