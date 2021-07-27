f533b1b4fbce8f173175806581be7a7e9a356e0a
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-27 15:22:17 +0600

    Solution - "1323. Maximum 69 Number"

---
+++ src/1323_maximum_69_number/csharp/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int Maximum69Number(int num)
+        {
+            char[] numChars = num.ToString().ToCharArray();
+
+            for (int i = 0; i < numChars.Length; i++)
+            {
+                if (numChars[i] == '6')
+                {
+                    numChars[i] = '9';
+                    break;
+                }
+            }
+
+            return int.Parse(new string(numChars));
+        }
+    }
+}
