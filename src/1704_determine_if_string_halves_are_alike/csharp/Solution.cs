20847fcde9d1c38a5777d45328f57faf9227a2ce
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-27 16:05:58 +0600

    Solution - "1704. Determine if String Halves Are Alike"

---
+++ src/1704_determine_if_string_halves_are_alike/csharp/Solution.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public bool HalvesAreAlike(string s)
+        {
+            int halfLen = s.Length / 2;
+            int vowelCount = 0;
+
+            for (int i = 0; i < halfLen; i++)
+            {
+                if (IsVowel(s[i]))
+                {
+                    vowelCount++;
+                }
+            }
+
+            for (int i = halfLen; i < s.Length; i++)
+            {
+                if (IsVowel(s[i]))
+                {
+                    vowelCount--;
+                }
+            }
+
+            return vowelCount == 0;
+        }
+
+        private bool IsVowel(char c)
+        {
+            switch (c)
+            {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                case 'A':
+                case 'E':
+                case 'I':
+                case 'O':
+                case 'U':
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
