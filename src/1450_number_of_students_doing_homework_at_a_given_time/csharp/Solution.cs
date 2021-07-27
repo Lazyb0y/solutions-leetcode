a39ce52a8a70ca90eb89107f9190cd3013f989e8
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-27 18:37:22 +0600

    Solution - "1450. Number of Students Doing Homework at a Given Time"

---
+++ src/1450_number_of_students_doing_homework_at_a_given_time/csharp/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int BusyStudent(int[] startTime, int[] endTime, int queryTime)
+        {
+            int studentCount = 0;
+
+            for (int i = 0; i < startTime.Length; i++)
+            {
+                if (startTime[i] <= queryTime && queryTime <= endTime[i])
+                {
+                    studentCount++;
+                }
+            }
+
+            return studentCount;
+        }
+    }
+}
