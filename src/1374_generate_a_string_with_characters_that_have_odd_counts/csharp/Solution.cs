ba355e75f4dd1ecc14b2c6606e648c010ce41be7
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-27 19:17:24 +0600

    Solution - "1374. Generate a String With Characters That Have Odd Counts"

---
+++ src/1374_generate_a_string_with_characters_that_have_odd_counts/csharp/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public string GenerateTheString(int n)
+        {
+            char[] stringChars = new char[n];
+
+            if (n % 2 != 0)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    stringChars[i] = 'a';
+                }
+            }
+            else
+            {
+                stringChars[0] = 'a';
+
+                for (int i = 1; i < n; i++)
+                {
+                    stringChars[i] = 'b';
+                }
+            }
+
+            return new string(stringChars);
+        }
+    }
+}
