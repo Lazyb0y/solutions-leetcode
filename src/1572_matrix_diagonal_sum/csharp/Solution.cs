678b0549f0d6be73f9e04abb99258f4af2972945
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-27 15:45:07 +0600

    Solution - "1572. Matrix Diagonal Sum"

---
+++ src/1572_matrix_diagonal_sum/csharp/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int DiagonalSum(int[][] mat)
+        {
+            int sum = 0;
+
+            int left = 0;
+            int right = mat[0].Length - 1;
+
+            for (int i = 0; i < mat.Length; i++, left++, right--)
+            {
+                sum += mat[i][left];
+
+                if (left != right)
+                {
+                    sum += mat[i][right];
+                }
+            }
+
+            return sum;
+        }
+    }
+}
