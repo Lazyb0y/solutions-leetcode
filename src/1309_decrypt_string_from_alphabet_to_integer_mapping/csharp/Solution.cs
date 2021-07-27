0ad67e5699d25e75508614def568796f39fcc3df
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-27 16:33:28 +0600

    Solution - "1309. Decrypt String from Alphabet to Integer Mapping"

---
+++ src/1309_decrypt_string_from_alphabet_to_integer_mapping/csharp/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public string FreqAlphabets(string s)
+        {
+            List<char> output = new List<char>();
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] == '#')
+                {
+                    output.Add((char) (int.Parse(s.Substring(i - 2, 2)) - 1 + 'a'));
+                    i -= 2;
+                }
+                else
+                {
+                    output.Add((char) (int.Parse(s[i].ToString()) - 1 + 'a'));
+                }
+            }
+
+            output.Reverse();
+            return new string(output.ToArray());
+        }
+    }
+}
