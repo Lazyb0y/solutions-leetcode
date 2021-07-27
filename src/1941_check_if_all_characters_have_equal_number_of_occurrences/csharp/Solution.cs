5d731c8ff8f7342217005d72f201a91c3ea2e3c4
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-28 00:13:25 +0600

    Solution - "1941. Check if All Characters Have Equal Number of Occurrences"

---
+++ src/1941_check_if_all_characters_have_equal_number_of_occurrences/csharp/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public bool AreOccurrencesEqual(string s)
+        {
+            Dictionary<char, int> occurrences = new Dictionary<char, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (occurrences.ContainsKey(s[i]))
+                {
+                    occurrences[s[i]]++;
+                }
+                else
+                {
+                    occurrences.Add(s[i], 1);
+                }
+            }
+
+            if (occurrences.Count == 1)
+            {
+                return true;
+            }
+
+            int baseCount = occurrences.ElementAt(0).Value;
+            for (int i = 1; i < occurrences.Values.Count; i++)
+            {
+                if (occurrences.ElementAt(i).Value != baseCount)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
