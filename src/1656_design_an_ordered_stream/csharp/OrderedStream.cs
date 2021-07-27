397be86f91d1c7de35725b9dac686e89b76d8c16
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-27 13:02:34 +0600

    Solution - "1656. Design an Ordered Stream"

---
+++ src/1656_design_an_ordered_stream/csharp/OrderedStream.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeSolutions
+{
+    public class OrderedStream
+    {
+        private readonly string[] _items;
+        private int _alreadySent;
+
+        public OrderedStream(int n)
+        {
+            _items = new string[n];
+        }
+
+        public IList<string> Insert(int idKey, string value)
+        {
+            _items[idKey - 1] = value;
+
+            int toTake = 0;
+            foreach (var item in _items)
+            {
+                if (item == null)
+                {
+                    break;
+                }
+
+                toTake++;
+            }
+
+            List<string> result = new List<string>();
+            for (int i = _alreadySent; i < toTake; i++)
+            {
+                result.Add(_items[i]);
+            }
+
+            _alreadySent = toTake;
+            return result;
+        }
+    }
+
+    /**
+     * Your OrderedStream object will be instantiated and called as such:
+     * OrderedStream obj = new OrderedStream(n);
+     * IList<string> param_1 = obj.Insert(idKey,value);
+     */
+}
