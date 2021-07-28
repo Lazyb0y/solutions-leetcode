9c4ebe1bae2e2615df4e8b303e3c980fa82fa81a
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-28 10:58:51 +0600

    Solution - "1436. Destination City"

---
+++ src/1436_destination_city/csharp/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public string DestCity(IList<IList<string>> paths)
+        {
+            string destination = null;
+            Dictionary<string, string> pathMap = new Dictionary<string, string>();
+
+            foreach (var path in paths)
+            {
+                pathMap.Add(path[0], path[1]);
+            }
+
+            destination = pathMap.ElementAt(0).Value;
+
+            while (pathMap.ContainsKey(destination))
+            {
+                destination = pathMap[destination];
+            }
+
+            return destination;
+        }
+    }
+}
