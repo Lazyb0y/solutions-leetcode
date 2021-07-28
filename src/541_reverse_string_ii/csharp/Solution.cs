26f133a47439178a040f3c8438d05c0424248eb8
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-28 09:52:44 +0600

    Solution - "541. Reverse String II"

---
+++ src/541_reverse_string_ii/csharp/Solution.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public string ReverseStr(string s, int k)
+        {
+            char[] reverseChars = new char[s.Length];
+
+            bool reverseMode = true;
+
+            for (int i = 0; i < s.Length; i = i + k)
+            {
+                int tail = (i + k < s.Length ? k : s.Length - i) + i;
+                if (reverseMode)
+                {
+                    int head = i;
+                    for (int j = tail - 1; j >= i; j--)
+                    {
+                        reverseChars[head] = s[j];
+                        head++;
+                    }
+                }
+                else
+                {
+                    for (int j = i; j < tail; j++)
+                    {
+                        reverseChars[j] = s[j];
+                    }
+                }
+
+                reverseMode = !reverseMode;
+            }
+
+            return new string(reverseChars);
+        }
+    }
+}
