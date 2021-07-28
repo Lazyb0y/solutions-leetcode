99eefc16ac1225dea442e586ca66ffc8e0f0bfa8
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-28 23:05:05 +0600

    Solution - "922. Sort Array By Parity II"

---
+++ src/922_sort_array_by_parity_ii/csharp/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int[] SortArrayByParityII(int[] nums)
+        {
+            int[] output = new int[nums.Length];
+            int evenPtr = 0;
+            int oddPtr = 1;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    output[evenPtr] = nums[i];
+                    evenPtr += 2;
+                }
+                else
+                {
+                    output[oddPtr] = nums[i];
+                    oddPtr += 2;
+                }
+            }
+            
+            return output;
+        }
+    }
+}
