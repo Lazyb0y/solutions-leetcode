fb2cd6affc8a4f0616a911c7fd9250f4cb4c89f9
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-28 23:26:31 +0600

    Solution - "26. Remove Duplicates from Sorted Array"

---
+++ src/26_remove_duplicates_from_sorted_array/csharp/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            int pointer = nums.Length;
+
+            for (int i = 1; i < pointer; i++)
+            {
+                if (nums[i - 1] == nums[i])
+                {
+                    pointer--;
+
+                    for (int j = i; j < pointer; j++)
+                    {
+                        nums[j] = nums[j + 1];
+                    }
+
+                    i--;
+                }
+            }
+
+            return pointer;
+        }
+    }
+}
