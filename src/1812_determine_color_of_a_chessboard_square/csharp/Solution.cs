14e95f0bccc27b7818d5444bc39a074be04a6e19
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-28 10:07:11 +0600

    Solution - "1812. Determine Color of a Chessboard Square"

---
+++ src/1812_determine_color_of_a_chessboard_square/csharp/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public bool SquareIsWhite(string coordinates)
+        {
+            if (((coordinates[0] - 'a' + 1) + (coordinates[1] - '0')) % 2 == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
