5a0570cf48b196a0dc774f890fc2a027189909ce
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-29 09:52:52 +0600

    Solution - "414. Third Maximum Number"

---
+++ src/414_third_maximum_number/csharp/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int ThirdMax(int[] nums)
+        {
+            HashSet<int> distinctNums = new HashSet<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                distinctNums.Add(nums[i]);
+            }
+
+            int firstMax = int.MinValue;
+            int secondMax = int.MinValue;
+            int thirdMax = int.MinValue;
+
+            for (int i = 0; i < distinctNums.Count; i++)
+            {
+                int element = distinctNums.ElementAt(i);
+                if (element > firstMax)
+                {
+                    thirdMax = secondMax;
+                    secondMax = firstMax;
+                    firstMax = element;
+                }
+                else if (element > secondMax)
+                {
+                    thirdMax = secondMax;
+                    secondMax = element;
+                }
+                else if (element > thirdMax)
+                {
+                    thirdMax = element;
+                }
+            }
+
+            if (distinctNums.Count < 3)
+            {
+                return firstMax;
+            }
+
+            return thirdMax;
+        }
+    }
+}
