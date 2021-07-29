5077d084a9f0d3617709f58cb3823c6093cb5c9e
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-29 13:12:44 +0600

    Solution - "202. Happy Number"

---
+++ src/202_happy_number/csharp/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public bool IsHappy(int n)
+        {
+            HashSet<int> uniqueNumbers = new HashSet<int>();
+
+            while (n != 1)
+            {
+                if (uniqueNumbers.Contains(n))
+                {
+                    return false;
+                }
+
+                uniqueNumbers.Add(n);
+
+                int sum = 0;
+                while (n != 0)
+                {
+                    sum = sum + (n % 10) * (n % 10);
+                    n /= 10;
+                }
+
+                n = sum;
+            }
+
+            return true;
+        }
+    }
+}
