c0125445ae3f100b4688731beadafbafd9471527
Foysal Ahamed <thisisfoysal@gmail.com>
2021-08-01 17:34:20 +0600

    Solution - "168. Excel Sheet Column Title"

---
+++ src/168_excel_sheet_column_title/csharp/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public string ConvertToTitle(int columnNumber)
+        {
+            string name = "";
+
+            while (columnNumber != 0)
+            {
+                char c = (char)((columnNumber - 1) % 26 + 'A');
+                name = c + name;
+                columnNumber = (columnNumber - 1) / 26;
+            }
+
+            return name;
+        }
+    }
+}
