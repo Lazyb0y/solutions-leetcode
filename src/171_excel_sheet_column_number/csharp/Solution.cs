18475731dc2118b699c16f6b0920600324d54e5b
Foysal Ahamed <thisisfoysal@gmail.com>
2021-08-01 18:56:51 +0600

    Solution - "171. Excel Sheet Column Number"

---
+++ src/171_excel_sheet_column_number/csharp/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int TitleToNumber(string columnTitle)
+        {
+            int number = 0;
+
+            for (int i = 0; i < columnTitle.Length; i++)
+            {
+                number = number * 26 + (columnTitle[i] - 'A' + 1);
+            }
+
+            return number;
+        }
+    }
+}
