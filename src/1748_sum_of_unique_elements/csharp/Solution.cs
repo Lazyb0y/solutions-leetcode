5ab31f2bd80117cc9793ae27d14b033b3879008f
Foysal Ahamed <thisisfoysal@gmail.com>
2021-08-01 16:47:43 +0600

    Solution - "1748. Sum of Unique Elements"

---
+++ src/1748_sum_of_unique_elements/csharp/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int SumOfUnique(int[] nums)
+        {
+            Dictionary<int, int> count = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (count.ContainsKey(nums[i]))
+                {
+                    count[nums[i]]++;
+                }
+                else
+                {
+                    count.Add(nums[i], 1);
+                }
+            }
+
+            int sum = 0;
+
+            foreach (var num in count)
+            {
+                if (num.Value == 1)
+                {
+                    sum += num.Key;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
