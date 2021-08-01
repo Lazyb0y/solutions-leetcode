a37f840497e2e2bdc5d57731ccd341443f31b4c8
Foysal Ahamed <thisisfoysal@gmail.com>
2021-08-01 17:01:54 +0600

    Solution - "1935. Maximum Number of Words You Can Type"

---
+++ src/1935_maximum_number_of_words_you_can_type/csharp/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int CanBeTypedWords(string text, string brokenLetters)
+        {
+            int count = 0;
+
+            string[] words = text.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = 0; j < words[i].Length; j++)
+                {
+                    if (brokenLetters.Contains(words[i][j].ToString()))
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+
+            return words.Length - count;
+        }
+    }
+}
