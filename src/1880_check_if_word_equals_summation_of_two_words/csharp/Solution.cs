7abb6b47fec40b7c2e04d2a602bb37a7fbdfedb5
Foysal Ahamed <thisisfoysal@gmail.com>
2021-08-08 15:44:28 +0600

    Solution - "1880. Check if Word Equals Summation of Two Words"

---
+++ src/1880_check_if_word_equals_summation_of_two_words/csharp/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public bool IsSumEqual(string firstWord, string secondWord, string targetWord)
+        {
+            int numOfFirstWord = 0;
+            int numOfSecondWord = 0;
+            int numOfTargetWord = 0;
+
+            for (int i = 0; i < firstWord.Length; i++)
+            {
+                numOfFirstWord = numOfFirstWord * 10 + (firstWord[i] - 'a');
+            }
+
+            for (int i = 0; i < secondWord.Length; i++)
+            {
+                numOfSecondWord = numOfSecondWord * 10 + (secondWord[i] - 'a');
+            }
+
+            for (int i = 0; i < targetWord.Length; i++)
+            {
+                numOfTargetWord = numOfTargetWord * 10 + (targetWord[i] - 'a');
+            }
+
+            return numOfFirstWord + numOfSecondWord == numOfTargetWord;
+        }
+    }
+}
