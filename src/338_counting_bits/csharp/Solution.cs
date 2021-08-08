6764c8161d06e16342b86d668efd2dd0b35562a9
Foysal Ahamed <thisisfoysal@gmail.com>
2021-08-08 16:53:25 +0600

    Solution - "338. Counting Bits"

---
+++ src/338_counting_bits/csharp/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int[] CountBits(int n)
+        {
+            int[] result = new int[n + 1];
+
+            for (int i = 0; i <= n; i++)
+            {
+                result[i] = DecimalToBinary(i).Count(c => c == '1');
+            }
+
+            return result;
+        }
+
+        private string DecimalToBinary(int num)
+        {
+            string binary = "";
+
+            while (num > 0)
+            {
+                binary = num % 2 + binary;
+                num /= 2;
+            }
+
+            return binary;
+        }
+    }
+}
