65d3cba1f8493adea3653267bfa5c0c7bdc514ca
Foysal Ahamed <thisisfoysal@gmail.com>
2021-08-08 16:11:00 +0600

    Solution - "1736. Latest Time by Replacing Hidden Digits"

---
+++ src/1736_latest_time_by_replacing_hidden_digits/csharp/Solution.cs
@@ -0,0 +1,56 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public string MaximumTime(string time)
+        {
+            char[] maxTime = new char[time.Length];
+            for (int i = 0; i < time.Length; i++)
+            {
+                if (time[i] == '?')
+                {
+                    switch (i)
+                    {
+                        case 0:
+                            if (time[1] == '?' || int.Parse(time[1].ToString()) < 4)
+                            {
+                                maxTime[0] = '2';
+                            }
+                            else
+                            {
+                                maxTime[0] = '1';
+                            }
+
+                            break;
+                        case 1:
+                            if (maxTime[0] == '2')
+                            {
+                                maxTime[1] = '3';
+                            }
+                            else
+                            {
+                                maxTime[1] = '9';
+                            }
+
+                            break;
+                        case 3:
+                            maxTime[3] = '5';
+                            break;
+                        case 4:
+                            maxTime[4] = '9';
+                            break;
+                        default:
+                            maxTime[i] = time[i];
+                            break;
+                    }
+                }
+                else
+                {
+                    maxTime[i] = time[i];
+                }
+            }
+
+            return new string(maxTime);
+        }
+    }
+}
