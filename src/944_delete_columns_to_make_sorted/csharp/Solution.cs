fcc1a37c93a0a62fc572968060885af523eec269
Foysal Ahamed <thisisfoysal@gmail.com>
2021-08-08 17:14:35 +0600

    Solution - "944. Delete Columns to Make Sorted"

---
+++ src/944_delete_columns_to_make_sorted/csharp/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCodeSolutions
+{
+    public class Solution
+    {
+        public int MinDeletionSize(string[] strs)
+        {
+            int columnToDelete = 0;
+
+            for (int j = 0; j < strs[0].Length; j++)
+            {
+                int charVal = -1;
+                for (int i = 0; i < strs.Length; i++)
+                {
+                    if (strs[i][j] < charVal)
+                    {
+                        columnToDelete++;
+                        break;
+                    }
+
+                    charVal = strs[i][j];
+                }
+            }
+
+            return columnToDelete;
+        }
+    }
+}
